f22c3dbc3a9a5206aa535e9840b8a58ecbd9acc0
krunalshastri00 <krunalshastri00@gmail.com>
2021-04-27 17:38:58 +0530

    Created build file and did some changes in package.json(backend)

--- Backend/app.js
+++ Backend/app.js
@@ -9,8 +9,13 @@ const keeperRouter = require("./Routes/Keeper_route");
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+if(process.env.MOD_ENV === "production"){
+    app.use(express.static("../Frontend/build"));
+}
+
 app.use("/keeperNotes",keeperRouter);
 
-app.listen(5000,function(){
-    console.log("Server is running on 5000!!");
+app.listen(process.env.PORT || 5000,function(){
+    console.log("Server is running!!");
 });
